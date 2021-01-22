5bf75b51f34e0ce397e65672344f1ce17c11f1a0
Sandy Kane <sakane@Sandys-MacBook-Pro.local>
2021-01-22 17:31:37 +0000

    Initial server commit

---
+++ server/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    }
+};
